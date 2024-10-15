127aa71b066467d12058b884a8792e4b9c04bfb9
Bruce Fitzsimons <Bruce@Fitzsimons.org>
2024-10-15 16:31:00 +1100

    More logging

--- components/goodnature_ble/goodnature_ble_listener.cpp
+++ components/goodnature_ble/goodnature_ble_listener.cpp
@@ -27,7 +27,7 @@ bool GoodnatureBleListener::parse_device(const esp32_ble_tracker::ESPBTDevice &d
   if (mfg_datas.size() > 0) {
     for (auto data : mfg_datas) {
         ESP_LOGW(TAG, " mfg adv datas - %s: (length %i)", data.uuid.to_string().c_str(), data.data.size());
-        //ESP_LOG_BUFFER_HEX_LEVEL(TAG, &data.data[0], data.data.size(), ESP_LOG_WARN);
+        ESP_LOG_BUFFER_HEX_LEVEL(TAG, &data.data[0], data.data.size(), ESP_LOG_WARN);
     }
   }
 
@@ -38,6 +38,8 @@ bool GoodnatureBleListener::parse_device(const esp32_ble_tracker::ESPBTDevice &d
       parse_kill_info(device.address_uint64(), kill_info);
       return true;
     }
+    ESP_LOGW(TAG, " mfg svc datas - %s: (length %i)", service_data.uuid.to_string().c_str(), service_data.data.size());
+    ESP_LOG_BUFFER_HEX_LEVEL(TAG, &service_data.data[0], service_data.data.size(), ESP_LOG_WARN);
   }
 
   return false;
